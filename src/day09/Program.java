023122380b9ace122d11ff2fe1392b76f83d922e
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-27 18:10:52 +0000

    Day 9: Added solution to part 1

---
+++ src/day09/Program.java
@@ -0,0 +1,85 @@
+package day09;
+
+import common.Utilities;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Find the shortest distance Santa can travel to deliver presents to
+ * several new locations
+ */
+public class Program
+{
+    public static void main(String[] args) throws IOException
+    {
+        List<String> inputData = Utilities.getFileLines("day09/input.txt");
+        Map<String, Map<String, Integer>> travelDistanceLookup = createLocationLookup(inputData);
+
+        List<Integer> allDistances = new ArrayList();
+        Set<String> locations = travelDistanceLookup.keySet();
+
+        for (String startLocation : locations)
+        {
+            Set<String> availableLocations = new TreeSet(locations);
+            availableLocations.remove(startLocation);
+            allDistances.addAll(findAllDistances(travelDistanceLookup, startLocation, availableLocations));
+        }
+
+        int shortestAvailableDistance = Collections.min(allDistances);
+        System.out.println(String.format("The shortest available distance is: %d", shortestAvailableDistance));
+    }
+
+    public static Map<String, Map<String, Integer>> createLocationLookup(List<String> inputData)
+    {
+        Map<String, Map<String, Integer>> locations = new HashMap();
+
+        for (String line: inputData)
+        {
+            String[] split = line.split(" ");
+            String locationOne = split[0];
+            String locationTwo = split[2];
+            int distance = Integer.parseInt(split[4]);
+
+            if (!locations.containsKey(locationOne)) locations.put(locationOne, new HashMap());
+            if (!locations.containsKey(locationTwo)) locations.put(locationTwo, new HashMap());
+
+            locations.get(locationOne).put(locationTwo, distance);
+            locations.get(locationTwo).put(locationOne, distance);
+        }
+
+        return locations;
+    }
+
+    public static List<Integer> findAllDistances(Map<String, Map<String, Integer>> distanceLookup,
+                                                 String currentLocation,
+                                                 Set<String> availableLocations)
+    {
+        List<Integer> allDistances = new ArrayList();
+
+        for (String destination : availableLocations)
+        {
+            int distanceTravelled = distanceLookup.get(currentLocation).get(destination);
+
+            Set<String> otherLocations = new TreeSet(availableLocations);
+            otherLocations.remove(destination);
+
+            List<Integer> childDistances = findAllDistances(distanceLookup, destination, otherLocations);
+
+            if (childDistances.size() == 0)
+            {
+                allDistances.add(distanceTravelled);
+                break;
+            }
+
+            for (int i = 0; i < childDistances.size(); i++)
+            {
+                childDistances.set(i, childDistances.get(i) + distanceTravelled);
+            }
+
+            allDistances.addAll(childDistances);
+        }
+
+        return allDistances;
+    }
+}
---
+++ test/day09/ProgramTest.java
@@ -0,0 +1,51 @@
+package day09;
+
+import common.Utilities;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest
+{
+    @Test
+    public void createLocationLookup() throws IOException
+    {
+        List<String> inputData = Utilities.getFileLines("day09/testinput.txt");
+        Map<String, Map<String, Integer>> locationLookup = Program.createLocationLookup(inputData);
+
+        assertEquals(141, (int)locationLookup.get("Belfast").get("Dublin"));
+        assertEquals(518, (int)locationLookup.get("Belfast").get("London"));
+        assertEquals(141, (int)locationLookup.get("Dublin").get("Belfast"));
+        assertEquals(464, (int)locationLookup.get("Dublin").get("London"));
+        assertEquals(518, (int)locationLookup.get("London").get("Belfast"));
+        assertEquals(464, (int)locationLookup.get("London").get("Dublin"));
+    }
+
+    @Test
+    public void findAllDistances() throws IOException
+    {
+        List<String> inputData = Utilities.getFileLines("day09/testinput.txt");
+        Map<String, Map<String, Integer>> locationLookup = Program.createLocationLookup(inputData);
+
+        Set<String> locations = new TreeSet(locationLookup.keySet());
+        locations.remove("London");
+        List<Integer> allDistances = Program.findAllDistances(locationLookup, "London", locations);
+        assertArrayEquals(new Integer[] {659, 605}, allDistances.toArray(new Integer[allDistances.size()]));
+
+        locations = new TreeSet(locationLookup.keySet());
+        locations.remove("Belfast");
+        allDistances = Program.findAllDistances(locationLookup, "Belfast", locations);
+        assertArrayEquals(new Integer[] {605, 982}, allDistances.toArray(new Integer[allDistances.size()]));
+
+        locations = new TreeSet(locationLookup.keySet());
+        locations.remove("Dublin");
+        allDistances = Program.findAllDistances(locationLookup, "Dublin", locations);
+        assertArrayEquals(new Integer[] {659, 982}, allDistances.toArray(new Integer[allDistances.size()]));
+    }
+}

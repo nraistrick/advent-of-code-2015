2f9cba05dc01d9d7e040ee788b8f319f1d91a585
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-28 12:26:15 +0000

    Day 12: Added solution to part 2

--- src/day12/Program.java
+++ src/day12/Program.java
@@ -1,13 +1,13 @@
 package day12;
 
-
 import common.Utilities;
 
 import java.io.IOException;
 
 /**
  * Calculate the sum of numbers in an erroneous javascript document produced
- * by software used by Santa's accounting-elves
+ * by software used by Santa's accounting-elves. Ignore any object (and its
+ * children) that contains a property with the value "red".
  */
 public class Program
 {
@@ -16,6 +16,11 @@ public class Program
         String document = Utilities.getFileContents("day12/input.txt");
         int numberCount = countAllNumbers(document);
         System.out.println("The total sum of all numbers in the document is: " + numberCount);
+
+        String cleanedDocument = cleanJson(document);
+        numberCount = countAllNumbers(cleanedDocument);
+        System.out.println("The total sum of all numbers in the cleaned document is: " + numberCount);
+
     }
 
     public static int countAllNumbers(String document)
@@ -39,5 +44,74 @@ public class Program
         }
 
         return total;
+    }
+
+    public static String cleanJson(String document)
+    {
+        String illegal = "red";
+
+        while (true)
+        {
+            int objectToRemoveIndex = document.indexOf(illegal);
+            if (objectToRemoveIndex == -1) break;
+
+            String context = document.substring(objectToRemoveIndex - 2, objectToRemoveIndex);
+            if (context.equals(",\"") || context.equals("[\""))
+            {
+                StringBuilder builder = new StringBuilder(document);
+                builder.replace(objectToRemoveIndex, objectToRemoveIndex + illegal.length(), "");
+                document = builder.toString();
+                continue;
+            }
+
+            document = removeContainingObject(document, objectToRemoveIndex);
+        }
+
+        return document;
+    }
+
+    public static String removeContainingObject(String document, int index)
+    {
+        StringBuilder builder = new StringBuilder(document);
+
+        int leftBraceIndex = getLeftEnclosingObjectBrace(document, index);
+        int rightBraceIndex = getRightEnclosingObjectBrace(document, index);
+        builder.replace(leftBraceIndex, rightBraceIndex + 1,"");
+
+        return builder.toString();
+    }
+
+    public static int getLeftEnclosingObjectBrace(String document, int index)
+    {
+        int braceCounter = 0;
+        int i;
+
+        for (i = index; i > 0; i--)
+        {
+            if      (document.charAt(i) == '{' && braceCounter == 0) break;
+            else if (document.charAt(i) == '}') braceCounter++;
+            else if (document.charAt(i) == '{') braceCounter--;
+        }
+
+        if (document.charAt(i) != '{') throw new IllegalArgumentException();
+
+        return i;
+    }
+
+    public static int getRightEnclosingObjectBrace(String document, int index)
+    {
+        int braceCounter = 0;
+        int i;
+
+        for (i = index; i < document.length(); i++)
+        {
+            if      (document.charAt(i) == '}' && braceCounter == 0) break;
+            else if (document.charAt(i) == '{') braceCounter++;
+            else if (document.charAt(i) == '}') braceCounter--;
+        }
+
+        if (document.charAt(i) != '}') throw new IllegalArgumentException();
+
+        return i;
     }
 }
--- test/day12/ProgramTest.java
+++ test/day12/ProgramTest.java
@@ -19,4 +19,42 @@ class ProgramTest
         assertEquals(0, Program.countAllNumbers("[]"));
     }
 
+    @Test
+    void cleanJson()
+    {
+        assertEquals("[1,2,3]",    Program.cleanJson("[1,2,3]"));
+        assertEquals("[1,,3]",     Program.cleanJson("[1,{c“:“red“,“b“:2},3]"));
+        assertEquals("",           Program.cleanJson("{“d“:“red“,“e“:[1,2,3,4],“f“:5}"));
+        assertEquals("[1,\"\",5]", Program.cleanJson("[1,\"red\",5]"));
+
+        assertEquals("[1,,2]", Program.cleanJson("[1,{“a”: {“b”:red}, ”c”: {“b”:red},”b”:”red”},2]"));
+        assertEquals("[1,,2]", Program.cleanJson("[1,{“a”: {“b”:red}, ”c”: {“b”:red},”b”:”red”},2]"));
+        assertEquals("[,2]",   Program.cleanJson("[{“b”:red},2]"));
+        assertEquals("[2,]",   Program.cleanJson("[2,{“b”:red}]"));
+        assertEquals("[\"\"]", Program.cleanJson("[\"red\"]"));
+        assertEquals("[1,,3]", Program.cleanJson("[1,{“a”: {“b”:2}, ”c”:”red“,“b“:2},3]"));
+        assertEquals("[1,{”c”:”hello”,”b”:2},3]", Program.cleanJson("[1,{”c”:”hello”,”b”:2},3]"));
+    }
+
+    @Test
+    void removeContainingObject()
+    {
+        assertEquals("[1,,3]",                           Program.removeContainingObject("[1,{“a”: {“b”:2}, ”c”:”red”,”b”:2},3]", 23));
+        assertEquals("[1,{“a”: , ”c”:”hello”,”b”:2},3]", Program.removeContainingObject("[1,{“a”: {“b”:red}, ”c”:”hello”,”b”:2},3]", 14));
+        assertEquals("[1,,2]", Program.removeContainingObject("[1,{“a”: {“b”:red}, ”c”: {“b”:red},”b”:”red”},2]", 38));
+    }
+
+    @Test
+    void getLeftEnclosingObjectBrace()
+    {
+        assertEquals(3, Program.getLeftEnclosingObjectBrace("[1,{“a”: {“b”:2}, ”c”:”red”,”b”:2},3]", 23));
+        assertEquals(9, Program.getLeftEnclosingObjectBrace("[1,{“a”: {“b”:red}, ”c”:”hello”,”b”:2},3]", 14));
+    }
+
+    @Test
+    void getRightEnclosingObjectBrace()
+    {
+        assertEquals(35, Program.getRightEnclosingObjectBrace("[1,{“a”: {“b”:2}, ”c”:{\"a\":1},\"b\":2},3]", 5));
+        assertEquals(17, Program.getRightEnclosingObjectBrace("[1,{“a”: {“b”:red}, ”c”:”hello”,”b”:2},3]", 14));
+    }
 }
